e524c643ac834b1366c6c68f2fe376ea6ada2ab4
koushik <koushik.dey@live.com>
2022-07-28 04:43:42 +0300

    crt

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -206,7 +206,7 @@ const Cart = () => {
               <SummaryTitle>ORDER SUMMARY</SummaryTitle>
               <SummaryItem>
                 <SummaryItemText>Subtotal</SummaryItemText>
-                <SummaryItemPrice>€ 80.00</SummaryItemPrice>
+                <SummaryItemPrice>$ {cart.total}</SummaryItemPrice>
               </SummaryItem>
               <SummaryItem>
                 <SummaryItemText>Estimated Shipping</SummaryItemText>
@@ -218,7 +218,7 @@ const Cart = () => {
               </SummaryItem>
                 <SummaryItem type="total">
                 <SummaryItemText>Total</SummaryItemText>
-                <SummaryItemPrice>€ 80.00</SummaryItemPrice>
+                <SummaryItemPrice>€ {cart.total}</SummaryItemPrice>
               </SummaryItem>
                 <Button>CHECKOUT NOW</Button>
            </Summary>
